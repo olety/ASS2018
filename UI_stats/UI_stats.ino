af21096419325a6c4669ae9104252f746000ef4b
Oleksii Kyrylchuk <olkyrylchuk@gmail.com>
2018-10-24 21:09:57 +0900

    Added the pet animation

--- UI_stats/UI_stats.ino
+++ UI_stats/UI_stats.ino
@@ -1,7 +1,43 @@
 #include <M5Stack.h>
 
+//Graphics
+#define living1 spider_living1_map
+#define living2 spider_living2_map
+#define playing1 spider_playing1_map
+#define playing2 spider_playing2_map
+#define eating1 spider_eating1_map
+#define eating2 spider_eating2_map
+#define sleeping1 spider_sleeping1_map
+#define sleeping2 spider_sleeping2_map
+#define dead1 spider_dead1_map
+#define dead2 spider_dead2_map
+
+
+#define livingArray1 extern unsigned char
+#define livingArray2 extern unsigned char
+#define playingArray1 extern unsigned char
+#define playingArray2 extern unsigned char
+#define eatingArray1 extern unsigned char
+#define eatingArray2 extern unsigned char
+#define sleepingArray1 extern unsigned char
+#define sleepingArray2 extern unsigned char
+#define deadArray1 extern unsigned char
+#define deadArray2 extern unsigned char
+
+livingArray1 living1[];
+livingArray2 living2[];
+playingArray1 playing1[];
+playingArray2 playing2[];
+eatingArray1 eating1[];
+eatingArray2 eating2[];
+sleepingArray1 sleeping1[];
+sleepingArray2 sleeping2[];
+deadArray1 dead1[];
+deadArray2 dead2[];
+
+
 // Game settings
-#define STARTING_SCORE 999990
+#define STARTING_SCORE 999995
 #define STARTING_SLEEP_PTS 50
 #define STARTING_HAPPINESS_PTS 50
 #define STARTING_HUNGER_PTS 50
@@ -30,6 +66,8 @@ int happiness_pts = STARTING_HAPPINESS_PTS;
 int hunger_pts = STARTING_HUNGER_PTS;
 int messageIndex = MSG_IDLE;
 int currentState = STATE_IDLE;
+
+TaskHandle_t idleHandle;
 
 String message[] = {
   "pet is waiting",
@@ -112,18 +150,12 @@ void displayStats(){
   M5.Lcd.printf("%d%%", happiness_pts);
 }
 
-void diplayPet() {
-  M5.Lcd.setTextColor(0xfd79);
-}
-
 void updateUI(){
-  // M5.Lcd.clear();
   displayTemp();
   displayMessage();
   displayScore();
   displayFunctions();
   displayStats();
-//  displayPet();
 }
 
 void updateScore(void *pvParameters) {
@@ -161,6 +193,7 @@ void displayFinalScreen() {
 
 void lose() {
   // TODO(UI ppl): show the resume button and the score 
+  
   messageIndex = MSG_DEATH;
   currentState = STATE_DEATH;
 }
@@ -198,12 +231,62 @@ void reset_game() {
   currentState = STATE_IDLE;
   updateUI();
 }
+
+void animation_idle(){
+  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,living1);
+  delay(500);
+//  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,living2);
+  delay(500);
+  updateUI();
+  }
+
+void animation_playing(){
+  updateUI();
+  M5.Lcd.fillRect(160,56,64,128,0xffff);
+  M5.Lcd.drawBitmap(96,56,64,128,playing1);
+  delay(250);
+//  updateUI();
+  M5.Lcd.fillRect(96,56,64,128,0xffff);
+  M5.Lcd.drawBitmap(160,56,64,128,playing2);
+  delay(250);
+  updateUI();
+  } 
+
+void animation_eating(){
+  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,eating1);
+  delay(250);
+  M5.Lcd.drawBitmap(96,56,128,128,eating2);
+  delay(250);
+  updateUI();
+  }
+
+void animation_sleeping(){
+  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,sleeping1);
+  delay(250);
+//  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,sleeping2);
+  delay(250);
+  updateUI();
+  }
+
+void animation_dead(){
+//  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,dead1);
+  delay(250);
+//  updateUI();
+  M5.Lcd.drawBitmap(96,56,128,128,dead2);
+  delay(250);
+//  updateUI();
+  }
   
 void processEvent(void *pvParameters) {
   // Handle a button press and update the score afterwards
   // We should also pause the updatePts activity during the event
   for (;;) {
-    // TODO(Binou) - Make it look fancy
     if (currentState == STATE_DEATH || currentState == STATE_WIN) {
       if (M5.BtnA.wasPressed() || M5.BtnB.wasPressed() || M5.BtnC.wasPressed()) {
         reset_game();
@@ -211,27 +294,38 @@ void processEvent(void *pvParameters) {
     } else {
       // Check if a button was pressed
       if (M5.BtnA.wasPressed()) {
+        vTaskSuspend( idleHandle );
         sleep_pts = sleep_pts + 5;
         messageIndex = MSG_SLEEP;
         currentState = STATE_SLEEP;
         checkDeath();
-        delay(1000);
+        animation_sleeping();
+        animation_sleeping();
+        
+        vTaskResume( idleHandle );
       }
       
       if (M5.BtnB.wasPressed()) {
+        vTaskSuspend( idleHandle );
         hunger_pts = hunger_pts - 5;
         messageIndex = MSG_HUNGER;
         currentState = STATE_HUNGER;
         checkDeath();
-        delay(1000);
+        animation_eating();
+        animation_eating();
+        vTaskResume( idleHandle );
+        M5.update();
       }
       
       if (M5.BtnC.wasPressed()) {
+        vTaskSuspend( idleHandle );
         happiness_pts = happiness_pts + 5;
         messageIndex = MSG_HAPPINESS;
         currentState = STATE_HAPPINESS;
         checkDeath();
-        delay(1000);
+        animation_playing();
+        animation_playing();
+        vTaskResume( idleHandle );
       }
       
       currentState = STATE_IDLE;
@@ -241,7 +335,15 @@ void processEvent(void *pvParameters) {
   }
 }
 
-
+void animateIdle(void *pvParameters) {
+  for (;;) {
+    if (currentState == STATE_DEATH || currentState == STATE_WIN) {
+      animation_dead();
+    } else {
+      animation_idle();
+    }
+  }
+}
   
 void updatePts() {
   // Every activity's points go down by 1 every second
@@ -280,6 +382,17 @@ void setup(){
       NULL,
       0
     );
+
+    xTaskCreatePinnedToCore (
+      animateIdle,
+      "animateIdle",
+      4096,
+      NULL,
+      3,
+      &idleHandle,
+      0
+    );
+    
    
 }
 
@@ -288,7 +401,6 @@ void setup(){
 void loop() {
   if (currentState != STATE_DEATH && currentState != STATE_WIN){
     updatePts();
-    updateUI();
     delay(1000);    
   }
 }
